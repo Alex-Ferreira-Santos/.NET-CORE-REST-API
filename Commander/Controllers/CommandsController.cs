733b3f5e1d1a0fc06c17911cf44b8286c3bbe992
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-07-30 13:36:52 -0300

    aplicando os metodos no controller

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Commander.Data;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +10,20 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        // GET api/commands
         [HttpGet]
         public ActionResult <IEnumerable<Command>> GetAllCommands()
         {
-
+            var commandItens = _repository.GetAppCommands();
+            return Ok(commandItens);
         }
-        [HttpGet]
+        //GET api/commands/id
+        [HttpGet("{id}")]
         public ActionResult <Command> GetCommandById(int id)
         {
-
+            var commandItem = _repository.GetcommandById(id);
+            return Ok(commandItem);
         }
     }
 }
