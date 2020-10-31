943b4fa60c7f04aa4a6a959c27cc8ec45bc1b641
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-10-31 11:57:51 -0300

    criação das classes

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -25,6 +27,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
---
+++ TodoApi/no-loc(Models)/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace TodoApi.no_loc(Models)
+{
+    public class TodoItem
+    {
+        public int id { get; set; }
+    }
+}
